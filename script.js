8b80e84216b19c3501658488ac062e8f0c01f44b
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-04-10 17:27:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-window.promises = [];
+window.promises = [promise1,promise2,promise3,promise4,promise5];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+const promise1 = new promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Race has started");
+	},Math.floor(Math.random()*5 + 1));
+});
+
+const promise2 = new promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Athletes running");
+	},Math.floor(Math.random()*5 + 1));
+});
+
+const promise3 = new promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("A competitive Race");
+	},Math.floor(Math.random()*5 + 1));
+});
+
+const promise4 = new promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Ahead of finish");
+	},Math.floor(Math.random()*5 + 1));
+});
+
+const promise5 = new promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Race has finished");
+	},Math.floor(Math.random()*5 + 1));
+});
+
+Promise.any(promises).then((value) =>{
+	document.getElementById('output').innerHTML = value;
+} );
